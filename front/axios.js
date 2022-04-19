c8092e3b94e60dcf988cbb05ae3c36b337f1c7ad
elouandelamer <44405561+elouandelamer@users.noreply.github.com>
2022-04-19 15:33:41 -0400

    Update axios.js

--- front/axios.js
+++ front/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const db = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "http://165.232.85.189:3001/",
   // baseURL: "http://6c5c-142-184-84-68.ngrok.io/recipes",
 });
 
