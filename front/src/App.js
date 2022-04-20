ea98d356ada6124b7df604ae2dccf0f5a516a244
tokismos <39965111+tokismos@users.noreply.github.com>
2022-04-20 17:30:43 -0400

    Update App.js

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,6 @@ import logo from "./logo.svg";
 import "./App.css";
 import { db } from "./axios";
 import { useState } from "react";
-import Input from "@mui/material/Input";
 
 function App() {
   const [name, setName] = useState();
